1b734f1e24c27349a3d36bbb0f53cbd3a0278c72
abstract-animation-9914 <vm930@hotmail.com>
2019-01-14 23:46:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+ if (array.length === 0)
+   puts "This block should not run!"
+   unless
+        if block_given?
+     new_array = []
+     i = 0
+          while i < array.length
+                if yield array[i] == true 
+                new_array.push(array[i])
+                end 
+          end
+    end
+      new_array
+    end
+   end 
+ end
 end
